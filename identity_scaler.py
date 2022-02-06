62834488f8020a4762f2f66c9500cb22324c22e9
thama23 <94192471+thama23@users.noreply.github.com>
2022-02-06 17:14:27 +0200

    initial

---
+++ identity_scaler.py
@@ -0,0 +1,31 @@
+from pysad.core.base_transformer import BaseTransformer
+
+
+class IdentityScaler(BaseTransformer):
+    """A scaler that does not modify the input, which is added for convenience.
+    """
+
+    def __init__(self):
+        super().__init__(-1)
+
+    def fit_partial(self, X):
+        """Convenience method that does not modify the input or the scaler.
+
+        Args:
+            X (np.float array of shape (num_features,)): Input feature vector.
+
+        Returns:
+            object: The scaler.
+        """
+        return self
+
+    def transform_partial(self, X):
+        """Convenience method that does not modify the input.
+
+        Args:
+            X (np.float array of shape (num_features,)): Input feature vector.
+
+        Returns:
+            X (np.float array of shape (features,)): The exact same input feature vector.
+        """
+        return X
