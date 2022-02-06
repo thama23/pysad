b785dbeae2ea59460ffa14c6c0f8cd53c4b17944
thama23 <94192471+thama23@users.noreply.github.com>
2022-02-06 17:14:28 +0200

    initial

---
+++ instance_unit_norm_scaler.py
@@ -0,0 +1,38 @@
+from pysad.core.base_transformer import BaseTransformer
+import numpy as np
+
+
+class InstanceUnitNormScaler(BaseTransformer):
+    """A scaler that makes the instance feature vector's norm equal to 1, i.e., the unit vector.
+
+    Args:
+        pow (float): The power, for which the norm is calculated. pow=2 is equivalent to the euclidean distance.
+    """
+
+    def __init__(self, pow=2):
+        super().__init__(-1)
+        self.pow = pow
+
+    def fit_partial(self, X):
+        """Fits particular (next) timestep's features to train the scaler.
+
+        Args:
+            X (np.float array of shape (num_features,)): Input feature vector.
+
+        Returns:
+            object: self.
+        """
+        return self
+
+    def transform_partial(self, X):
+        """Scales particular (next) timestep's vector.
+
+        Args:
+            X (np.float array of shape (num_features,)): Input feature vector.
+
+        Returns:
+            scaled_X (np.float array of shape (features,)): Scaled feature vector.
+        """
+        X_norm = np.linalg.norm(X, ord=self.pow)
+
+        return X / X_norm
